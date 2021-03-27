f68a4b5d0769093db74d486a9f5565c64ae63620
vmoeak <dangpengfei123@gmail.com>
2021-03-27 21:23:44 +0800

    webpack svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="nav">
-    <router-link to="/money">money</router-link>|
-    <router-link to="/labels">labels</router-link>|
+    <router-link to="/money">
+      money
+      <svg>
+        <use xlink:href="#label"></use>
+      </svg> </router-link
+    >| <router-link to="/labels">labels</router-link>|
     <router-link to="/statistic">statistic</router-link>
   </div>
 </template>
 
 <script lang="ts">
+// import x from "@/assets/icons/label.svg";
+// console.log(x);
 export default {
   name: "Nav",
 };
